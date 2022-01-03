acf1feec9008091e04d129ab03cedd69c044c09d
Loris Cro <kappaloris@gmail.com>
2022-01-03 13:32:43 +0100

    Initial Commit

---
+++ ZigAdventures/ZigAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZigAdventuresApp.swift
+//  ZigAdventures
+//
+//  Created by Loris Cro on 03/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ZigAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
